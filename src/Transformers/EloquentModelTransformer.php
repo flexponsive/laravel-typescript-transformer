79386ae5391c29633c6d3d54ed286d25f2324e06
basteln3rk <gnutzmann@flexponsive.net>
2023-10-22 15:48:12 +0000

    cleanup

--- src/Transformers/EloquentModelTransformer.php
+++ src/Transformers/EloquentModelTransformer.php
@@ -4,31 +4,27 @@ declare(strict_types=1);
 
 namespace Spatie\LaravelTypeScriptTransformer\Transformers;
 
+use \Illuminate\Support\Str;
+use \phpDocumentor\Reflection\Type;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Schema;
-use \phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Fqsen;
 use ReflectionClass;
+use Spatie\TypeScriptTransformer\Structures\MissingSymbolsCollection;
 use Spatie\TypeScriptTransformer\Structures\TransformedType;
 use Spatie\TypeScriptTransformer\Transformers\Transformer;
-use Spatie\TypeScriptTransformer\TypeScriptTransformerConfig;
-use Spatie\TypeScriptTransformer\Structures\MissingSymbolsCollection;
 use Spatie\TypeScriptTransformer\Transformers\TransformsTypes;
 use Spatie\TypeScriptTransformer\TypeProcessors\ReplaceDefaultsTypeProcessor;
-use \Illuminate\Support\Str;
-use phpDocumentor\Reflection\Fqsen;
-use \phpDocumentor\Reflection\TypeResolver;
+use Spatie\TypeScriptTransformer\TypeScriptTransformerConfig;
 
-class EloquentModelTransformer  implements Transformer
+class EloquentModelTransformer implements Transformer
 {
     use TransformsTypes;
     protected TypeScriptTransformerConfig $config;
 
-    public function __construct(TypeScriptTransformerConfig $config = null)
+    public function __construct(TypeScriptTransformerConfig $config)
     {
-        if ($config) {
-            $this->config = $config;
-        }
+        $this->config = $config;
     }
 
     public function canTransform(ReflectionClass $class): bool
@@ -38,7 +34,7 @@ class EloquentModelTransformer  implements Transformer
 
     public function transform(ReflectionClass $class, string $name): ?TransformedType
     {
-        if (!$this->canTransform($class)) {
+        if (! $this->canTransform($class)) {
             return null;
         }
 
@@ -57,7 +53,7 @@ class EloquentModelTransformer  implements Transformer
         );
     }
 
-    public function getPublicRelations(ReflectionClass $class) : Collection
+    public function getPublicRelations(ReflectionClass $class): Collection
     {
         /* @var EloquentModel */
         $instance = $class->newInstance();
@@ -69,7 +65,7 @@ class EloquentModelTransformer  implements Transformer
             $showModelCommand,
             $instance
         );
-        
+
         // TODO allow hiding relations through docstring or similar
 
         return $res;
@@ -83,7 +79,7 @@ class EloquentModelTransformer  implements Transformer
             $this->getPublicRelations($class)->toArray(),
             function (string $carry, $relation) use ($missingSymbols, $class) {
 
-                $instanceType = new \phpDocumentor\Reflection\Types\Object_(new Fqsen("\\" . $relation['related']));
+                $instanceType = new \phpDocumentor\Reflection\Types\Object_(new Fqsen('\\' . $relation['related']));
                 $transformedType = $this->typeToTypeScript(
                     $instanceType,
                     $missingSymbols,
@@ -91,14 +87,14 @@ class EloquentModelTransformer  implements Transformer
                 );
 
                 $isMany = Str::endsWith($relation['type'], 'Many');
+
                 return $isMany
-                ? "{$carry}{$relation['name']}?: Array<{$transformedType}>;" . PHP_EOL
-                :"{$carry}{$relation['name']}?: {$transformedType};" . PHP_EOL;
+                    ? "{$carry}{$relation['name']}?: Array<{$transformedType}>;" . PHP_EOL
+                    : "{$carry}{$relation['name']}?: {$transformedType};" . PHP_EOL;
             },
             ''
         );
     }
-
 
     public function getPublicAttributes(ReflectionClass $class): Collection
     {
@@ -111,16 +107,7 @@ class EloquentModelTransformer  implements Transformer
         $res = $showModelCommandReflection->getMethod('getAttributes')->invoke(
             $showModelCommand,
             $instance
-        )->filter(fn ($attr) => $attr["hidden"] !== true)
-            ->map(function ($attr) {
-                /*
-                // extract the native db field type from "bigint unsigned" or "string(255)"
-                preg_match('/^([^ \(]+)/', $attr["type"], $matches);
-                $dbFieldType = $matches[1];
-                $attr['db_field_type'] = $dbFieldType;
-                */
-                return $attr;
-            });
+        )->filter(fn ($attr) => $attr['hidden'] !== true);
 
         return $res;
     }
@@ -133,9 +120,14 @@ class EloquentModelTransformer  implements Transformer
         return array_reduce(
             $this->getPublicAttributes($class)->toArray(),
             function (string $carry, $property) use ($missingSymbols, $class) {
-                //dd($property);
+                $type = null;
+                if ($property['cast']) {
+                    $type = $this->inferTypeFromCast($property['cast']);
+                } elseif ($property['type']) {
+                    $type = $this->inferTypeFromDbFieldDescription($property['type']);
+                }
                 $transformedType = $this->typeToTypeScript(
-                    $this->castToPhpdocumentorType($property['cast'] ?? "encrypted"),
+                    $type ?? new \phpDocumentor\Reflection\Types\Mixed_(),
                     $missingSymbols,
                     $class->getName(),
                 );
@@ -150,61 +142,75 @@ class EloquentModelTransformer  implements Transformer
         );
     }
 
-    public function castToPhpdocumentorType(string $castedAs) : Type
+    public function inferTypeFromCast(string $castedAs): Type
     {
         if (Str::startsWith($castedAs, 'encrypted:')) {
             $castedAs = Str::replaceFirst('encrypted:', '', $castedAs);
         }
         if (Str::contains($castedAs, ':')) {
-            $castedAs = explode(":", $castedAs)[0];
+            $castedAs = explode(':', $castedAs)[0];
         }
 
         /* @var \phpDocumentor\Reflection\Type */
         $phpdocumentorType = null;
         switch ($castedAs) {
-            case "datetime":
-            case "immutable_datetime":
-            case "date":
-            case "immutable_date":
-            case "hashed":
-            case "Illuminate\Database\Eloquent\Casts\AsStringable":
-            case "AsStringable":
-            case "decimal":
-            case "encrypted":
+            case 'datetime':
+            case 'immutable_datetime':
+            case 'date':
+            case 'immutable_date':
+            case 'hashed':
+            case 'Illuminate\Database\Eloquent\Casts\AsStringable':
+            case 'AsStringable':
+            case 'decimal':
+            case 'encrypted':
                 $phpdocumentorType = new \phpDocumentor\Reflection\Types\String_();
+
                 break;
-            case "array":
+            case 'array':
                 $phpdocumentorType = new \phpDocumentor\Reflection\Types\Array_();
+
                 break;
-            case "boolean":
+            case 'boolean':
                 $phpdocumentorType = new \phpDocumentor\Reflection\Types\Boolean();
+
                 break;
-            case "double":
-            case "float":
+            case 'double':
+            case 'float':
                 $phpdocumentorType = new \phpDocumentor\Reflection\Types\Float_();
+
                 break;
-            case "int":
-            case "integer":
-            case "timestamp":
+            case 'int':
+            case 'integer':
+            case 'timestamp':
                 $phpdocumentorType = new \phpDocumentor\Reflection\Types\Integer();
+
                 break;
-            case "object":
+            case 'object':
                 $phpdocumentorType = new \phpDocumentor\Reflection\Types\Object_();
+
                 break;
             default:
-                // echo "castedAS=" . $castedAs;
-                $phpdocumentorType = new \phpDocumentor\Reflection\Types\Object_(new Fqsen("\\" . $castedAs));
-            break;
+                // echo 'castedAS=' . $castedAs;
+                $phpdocumentorType = new \phpDocumentor\Reflection\Types\Object_(new Fqsen('\\' . $castedAs));
+
+                break;
         }
+
         return $phpdocumentorType;
     }
 
-    protected function typeProcessors(): array
+    public function inferTypeFromDbFieldDescription($dbFieldDescription): ?Type
     {
-        return [
-            new ReplaceDefaultsTypeProcessor(
-                $this->config->getDefaultTypeReplacements()
-            ),
-        ];
+        preg_match('/^([^ \(]+)/', $dbFieldDescription, $matches);
+        $dbFieldType = $matches[1];
+
+        if ($dbFieldType == 'string') {
+            return new \phpDocumentor\Reflection\Types\String_();
+        } elseif (Str::endsWith($dbFieldType, 'int')) {
+            return new \phpDocumentor\Reflection\Types\Integer();
+        } else {
+            return null;
+        }
     }
+
 }
